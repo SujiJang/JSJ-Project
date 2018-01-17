7ce287e4ec7e513e344abd90cba982bf4a3ac490
user1 <user1@KH_G>
2018-01-17 14:20:44 +0900

    update table2

--- JSJ-Project/src/main/java/config/shoppingsql.sql
+++ JSJ-Project/src/main/java/config/shoppingsql.sql
@@ -24,15 +24,15 @@ create table members(
    	m_phonenum varchar2(30) not null,
    	m_birth varchar2(30) not null,
    	m_gen varchar2(10) not null,
-   	m_date date(20) not null,
-   	m_point number(50)
+   	m_date date not null,
+   	m_point number(30)
 );
 
 create sequence members_seq start with 1 increment by 1;
 
 ---------------------------------------------------------------------------------------
 
-create mainproducts(
+create table mainproducts(
 	mp_num number(20) constraint mainproducts_num_pk primary key,
  	mp_name varchar2(50) not null,
  	mp_main_category varchar2(50) not null,
@@ -47,7 +47,7 @@ create mainproducts(
 create sequence mainproducts_seq start with 1 increment by 1;
 
 ------------------------------------------------------------------------------
-
+drop table sumnail;
 create table sumnail(
 	sum_num number(30) constraint sumnail_num_pk primary key,
 	mp_num number(20) not null,
@@ -56,21 +56,22 @@ create table sumnail(
 	sum_third varchar2(30),
 	sum_forth varchar2(30),
 	sum_fifth varchar2(30),
-	constraint sumnail_mp_num_fk foreign key(mp_num) references mainproducts(mp_num);
+	constraint sumnail_mp_num_fk foreign key(mp_num) references mainproducts(mp_num)
 );
 
 create sequence sumnail_seq start with 1 increment by 1;
 
+drop table products;
 -------------------------------------------------------------------------------------------------------
 create table products(
-	p_num number(10) constraint sumnail_num_pk primary key,
+	p_num number(10) constraint products_num_pk primary key,
 	mp_num number(10) not null,
 	p_color varchar2(20) not null,
 	p_size varchar2(20) not null,
 	p_date date not null,
 	p_amount number(10),
 	p_order_count number(10) default 0,
-	constraint products_mp_num_fk foreign key(mp_num) references mainproducts(mp_num);
+	constraint products_mp_num_fk foreign key(mp_num) references mainproducts(mp_num)
 );
 
 create sequence products_seq start with 1 increment by 1;
@@ -90,11 +91,11 @@ create table orders(
 	o_address varchar2(100) not null,
 	o_phone varchar2(50),
 	o_payment varchar2(50),
-	o_paycheck boolean default false,
+	o_paycheck varchar2(10) default 'F',
 	o_etc varchar2(50),
 	o_message varchar2(100),
 	constraint orders_mp_num_fk foreign key(mp_num) references mainproducts(mp_num),
-	constraint orders_m_id_fk foreign key(m_id) references members(m_id);
+	constraint orders_m_id_fk foreign key(m_id) references members(m_id)
 );
 
 create sequence orders_seq start with 1 increment by 1;
@@ -105,10 +106,10 @@ create table basket(
 	m_id varchar2(20) not null,
 	mp_num number(10) not null,
 	b_inform varchar2(100),
-	b_price number(50),
+	b_price number(30),
 	b_amount number(20),
 	constraint basket_m_id_fk foreign key(m_id) references members(m_id),
-	constraint basket_mp_num_fk foreign key(mp_num) references mainproducts(mp_num);
+	constraint basket_mp_num_fk foreign key(mp_num) references mainproducts(mp_num)
 );
 
 create sequence basket_seq start with 1 increment by 1;
@@ -120,10 +121,10 @@ create table mine(
 	m_id varchar2(20) not null,
 	mp_num number(10) not null,
 	mi_inform varchar2(100),
-	mi_price number(50),
+	mi_price number(30),
 	mi_amount number(20),
 	constraint mine_m_id_fk foreign key(m_id) references members(m_id),
-	constraint mine_mp_num_fk foreign key(mp_num) references mainproducts(mp_num);
+	constraint mine_mp_num_fk foreign key(mp_num) references mainproducts(mp_num)
 );
 
 create sequence mine_seq start with 1 increment by 1;
@@ -135,13 +136,13 @@ create table delivery(
 	de_num number(10) constraint delivery_num_pk primary key,
 	order_code varchar2(50) not null,
 	m_id varchar2(20) not null,
-	de_price number(50),
+	de_price number(30),
 	de_date date,
-	constraint delivery_m_id_fk foreign key(m_id) references members(m_id),
-	constraint delivery_order_code_fk foreign key(order_code) references orders(order_code);
+	constraint delivery_m_id_fk foreign key(m_id) references members(m_id)
+
 );
 
 create sequence delivery_seq start with 1 increment by 1;
 
 =================================================================================
-
+	constraint delivery_order_code_fk foreign key(order_code) references orders(order_code)
